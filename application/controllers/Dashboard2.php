4443fc7258da806adf4ffe5a2eae78a5ad2ef5d1
Susantokun <susantokun.com@gmail.com>
2019-02-20 04:28:50 +0700

    Version 1.1

---
+++ application/controllers/Dashboard2.php
@@ -0,0 +1,31 @@
+<?php
+#===================================================|
+# Please DO NOT modify this information :			      |
+#---------------------------------------------------|
+# @Author 		: Susantokun
+# @Date 		  : 2018-05-25T06:25:59+07:00
+# @Email 		  : susantokun.com@gmail.com
+# @Project 		: CodeIgniter
+# @Filename 	: Dashboard2.php
+# @Instagram 	: susantokun
+# @Website 		: http://www.susantokun.com
+# @Youtube 		: http://youtube.com/susantokun
+# @Last modified time: 2018-05-25T06:32:04+07:00
+#===================================================|
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard2 extends CI_Controller{
+
+  public function __construct()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+  }
+
+  function index()
+  {
+    $this->load->view('dashboard2');
+  }
+
+}
---
+++ application/views/template/_meta.php
@@ -0,0 +1,4 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- Tell the browser to be responsive to screen width -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
