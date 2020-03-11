50963658f1ac3a4f19d681d9f6baea45c3074c6a
susantokun <admin@susantokun.com>
2020-03-12 05:29:48 +0700

    upgrade version codeigniter to 3.1.11

--- application/views/template/_meta.php
+++ application/views/template/_meta.php
@@ -2,3 +2,4 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <!-- Tell the browser to be responsive to screen width -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href='<?= base_url("assets/uploads/images/avatar.png"); ?>' rel='shortcut icon' type='image/x-icon' />
