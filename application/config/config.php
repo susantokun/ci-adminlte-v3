158ae008e42a185a6f24e267b48bb33d294d6647
susantokun <admin@susantokun.com>
2020-03-12 05:04:32 +0700

    update base_url dynamic

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
  */
-$config['base_url'] = 'http://localhost/ci-susantokun_adminlte-v3-alpha/';
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http');
+$config['base_url'] .= '://'.$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +38,7 @@ $config['base_url'] = 'http://localhost/ci-susantokun_adminlte-v3-alpha/';
 | variable so that it is blank.
 |
  */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
